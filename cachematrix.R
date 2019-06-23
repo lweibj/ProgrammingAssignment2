91d2c9ee3dcbad668e529fa8e70c90ba22adae7a
Liu Wei <lwlwei@cn.ibm.com>
2019-06-23 20:02:32 +0800

    cache matrix for Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matrx <- NULL
+        set <- function(y) {
+                x <<- y
+                matrx <<- NULL
+        }
+        get <- function() x
+        setMatrix <- function(m) matrx <<- m
+        getMatrix <- function() matrx
+        list(set = set,
+             get = get,
+             setMatrix = setMatrix,
+             getMatrix = getMatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      mat_local <- x$getMatrix()
+      if (!is.null(mat_local)) {
+              message("getting cached data")
+              return(mat_local)
+      }
+      mat <- x$get()
+      mat_local <- solve(mat, ...)
+      x$setMatrix(mat_local)
+      mat_local
 }
